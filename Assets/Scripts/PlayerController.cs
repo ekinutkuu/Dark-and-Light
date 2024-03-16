5ec15a80ebe46a0726bff624ec12d633c5c87103
Ekin Utku <ekinutkugulmez@gmail.com>
2024-03-16 18:51:53 +0300

    added: character movement animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,10 +11,12 @@ public class PlayerController : MonoBehaviour
     Quaternion targetRotation;
 
     CameraController cameraController;
+    Animator animator;
 
     private void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -22,13 +24,13 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        float isMoving = Mathf.Abs(h) + Mathf.Abs(v);
+        float moveAmount = Mathf.Clamp01(Mathf.Abs(h) + Mathf.Abs(v));
 
         var moveInput = (new Vector3(h, 0, v)).normalized;
 
         var moveDirection = cameraController.PlanerRotation * moveInput;
 
-        if ( isMoving > 0 )
+        if (moveAmount > 0 )
         {
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
             targetRotation = Quaternion.LookRotation(moveDirection);
@@ -36,5 +38,6 @@ public class PlayerController : MonoBehaviour
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
+        animator.SetFloat("moveAnim", moveAmount, 0.2f, Time.deltaTime);
     }
 }
